63d76bdccb8feaea082df306fcf3af630a48b32f
Mark Harfouche <mark.harfouche@gmail.com>
2018-10-28 19:56:12 -0400

    don't use memory mapped file. It is actually quite slow since it copies to RAM, then to disk

--- redpil/bmp.py
+++ redpil/bmp.py
@@ -40,55 +40,46 @@ def imwrite(filename, image):
 
     if image.dtype != np.uint8:
         raise NotImplementedError('Only uint8 images are supported.')
+    header = np.zeros(1, dtype=header_t)
+    info_header = np.empty(1, dtype=info_header_t)
+
+    header['signature'] = 'BM'.encode()
+
+    bits_per_pixel = image.itemsize * 8
+    # Not correct for color images
+    # BMP wants images to be padded to a multiple of 4
+    row_size = (bits_per_pixel * image.shape[1] + 31) // 32 * 4
+    image_size = row_size * image.shape[0]
+
+
+    header['file_offset_to_pixelarray'] = (header.nbytes +
+                                           info_header.nbytes +
+                                           gray_color_table.nbytes)
+
+    header['filesize'] = (header['file_offset_to_pixelarray'] + image_size)
+
+    info_header['header_size'] = info_header.nbytes
+    info_header['image_width'] = image.shape[1]
+    # A positive height states the the array is saved "bottom to top"
+    # A negative height states that the array is saved "top to bottom"
+    # Top to bottom has a larger chance of being contiguous in C memory
+    info_header['image_height'] = -image.shape[0]
+    info_header['image_planes'] = 1
+    info_header['bits_per_pixel'] = bits_per_pixel
+    info_header['compression'] = compression_types.index('BI_RGB')
+    info_header['image_size'] = 0
+    info_header['x_pixels_per_meter'] = 0
+    info_header['y_pixels_per_meter'] = 0
+    info_header['colors_in_color_table'] = 0
+    info_header['important_color_count'] = 0
 
     with open(filename, 'bw+') as f:
-        offset = 0
-        header = np.memmap(f, dtype=header_t, mode='w+', shape=1,
-                           offset=offset)
-        offset += header.nbytes
-
-        info_header = np.memmap(f, dtype=info_header_t, mode='w+', shape=1,
-                                offset=offset)
-        offset += info_header.nbytes
-
-        color_table = np.memmap(f, dtype=gray_color_table.dtype, mode='w+',
-                                shape=gray_color_table.shape, offset=offset)
-        offset += color_table.nbytes
-
-        header['signature'] = 'BM'.encode()
-
-        bits_per_pixel = image.itemsize * 8
-        # Not correct for color images
-        # BMP wants images to be padded to a multiple of 4
-        row_size = (bits_per_pixel * image.shape[1] + 31) // 32 * 4
-        image_size = row_size * image.shape[0]
-
-
-        header['file_offset_to_pixelarray'] = (header.nbytes +
-                                               info_header.nbytes +
-                                               color_table.nbytes)
-
-        header['filesize'] = (header['file_offset_to_pixelarray'] + image_size)
-
-        info_header['header_size'] = info_header.nbytes
-        info_header['image_width'] = image.shape[1]
-        # A positive height states the the array is saved "bottom to top"
-        # A negative height states that the array is saved "top to bottom"
-        # Top to bottom has a larger chance of being contiguous in C memory
-        info_header['image_height'] = -image.shape[0]
-        info_header['image_planes'] = 1
-        info_header['bits_per_pixel'] = bits_per_pixel
-        info_header['compression'] = compression_types.index('BI_RGB')
-        info_header['image_size'] = 0
-        info_header['x_pixels_per_meter'] = 0
-        info_header['y_pixels_per_meter'] = 0
-        info_header['colors_in_color_table'] = 0
-        info_header['important_color_count'] = 0
-
-        color_table[...] = gray_color_table
-        
-        f.seek(offset)
+        f.write(header)
+        f.write(info_header)
+        f.write(gray_color_table)
         if row_size == image.shape[1]:
+            # Small optimization when the image is a multiple of 4 bytes
+            # it actually avoids a full memory copy, so it is quite useful
             f.write(np.ascontiguousarray(image).data)
         else:
             # Now slice just the part of the image that we actually write to.
