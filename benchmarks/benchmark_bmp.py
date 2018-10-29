61cd5935402d6ca216a8d949ebceb9c269121e5e
Mark Harfouche <mark.harfouche@gmail.com>
2018-10-28 20:41:30 -0400

    Add some benchmarks for imread. for the limited case I am interested in, we are about 4x faster.

--- benchmarks/benchmark_bmp.py
+++ benchmarks/benchmark_bmp.py
@@ -3,7 +3,7 @@
 import numpy as np
 from PIL import Image
 from tempfile import mkdtemp
-from redpil.bmp import imwrite
+from redpil.bmp import imwrite, imread
 from pathlib import Path
 import shutil
 
@@ -12,13 +12,17 @@ class BMPSuite:
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
-    params = ([(128, 128), (1024, 1024), (2048, 4096), (32768, 32768)],
+    params = ([(128, 128), (1024, 1024),
+               (2048, 4096), (2**5 * 1024, 2 ** 5 *1024)],
               ['pillow', 'redpil'])
     param_names = ['shape', 'mode']
 
     def setup(self, shape, mode):
         self.img = np.random.randint(255, size=shape, dtype=np.uint8)
         self.tmpdir = Path(mkdtemp())
+        self.filename = self.tmpdir / 'saved.bmp'
+        imwrite(self.filename, self.img)
+
 
     def time_save(self, shape, mode):
         if mode == 'pillow':
@@ -28,6 +32,15 @@ class BMPSuite:
         else:
             filename = self.tmpdir / 'redpil.bmp'
             imwrite(filename, self.img)
+
+    def time_load(self, shape, mode):
+        if mode == 'pillow':
+            try:
+                np.asarray(Image.open(self.filename))
+            except Image.DecompressionBombError:
+                pass
+        else:
+            imread(self.filename)
 
     def teardown(self, shape, mode):
         shutil.rmtree(self.tmpdir)
