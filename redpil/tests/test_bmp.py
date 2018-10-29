2063eb153b556a01e9d7ceaf67b0b62fd0741baa
Mark Harfouche <mark.harfouche@gmail.com>
2018-10-28 21:45:00 -0400

    try to fix python 3.5 tests again.

--- redpil/tests/test_bmp.py
+++ redpil/tests/test_bmp.py
@@ -10,7 +10,7 @@ from numpy.testing import assert_array_equal
 import os
 
 def test_failers(tmpdir):
-    tmpfile = os.path.join(tmpdir, 'test.bmp')
+    tmpfile = os.path.join(str(tmpdir), 'test.bmp')
     img = np.zeros((4, 4), dtype=np.float32)
     with pytest.raises(NotImplementedError):
         imwrite(tmpfile, img)
@@ -22,7 +22,7 @@ def test_failers(tmpdir):
 @pytest.mark.parametrize('shape', [(4, 4), (7, 7), (21, 7)])
 @pytest.mark.parametrize('backend', ['pillow', 'redpil'])
 def test_zero_image(tmpdir, shape, backend):
-    tmpfile = os.path.join(tmpdir, 'test.bmp')
+    tmpfile = os.path.join(str(tmpdir), 'test.bmp')
 
     img = np.random.randint(255, size=shape, dtype=np.uint8)
     imwrite(tmpfile, img)
