14ee73c09c420617ff71f4148e5202c773d5852e
Mark Harfouche <mark.harfouche@gmail.com>
2018-10-30 12:36:32 -0400

    Benchmarks were wrong. Imageio is quite slow at 8bpp

--- benchmarks/benchmark_bmp.py
+++ benchmarks/benchmark_bmp.py
@@ -33,13 +33,14 @@ class BMPSuite8bpp:
     def time_load(self, shape, mode):
         if mode == 'pillow':
             try:
-                np.asarray(Image.open(self.filename).convert('L'))
+                img = np.asarray(Image.open(self.filename).convert('L'))
             except Image.DecompressionBombError:
                 pass
         elif mode == 'imageio':
-            imread(self.filename)
+            img = imageio.imread(self.filename)
         else:
-            imread(self.filename)
+            img = imread(self.filename)
+        assert np.array_equal(img, self.img)
 
     def teardown(self, shape, mode):
         shutil.rmtree(self.tmpdir)
@@ -73,13 +74,61 @@ class BMPSuite24bpp:
     def time_load(self, shape, mode):
         if mode == 'pillow':
             try:
-                np.asarray(Image.open(self.filename).convert('L'))
+                img = np.asarray(Image.open(self.filename).convert('RGB'))
             except Image.DecompressionBombError:
                 pass
         elif mode == 'imageio':
-            imread(self.filename)
+            img = imageio.imread(self.filename)
         else:
-            imread(self.filename)
+            img = imread(self.filename)
+
+        assert np.array_equal(img, self.img)
+
+
+    def teardown(self, shape, mode):
+        shutil.rmtree(self.tmpdir)
+
+
+class BMPSuite32bpp:
+    params = ([(32, 128, 4), (256, 1024, 4),
+               (2048, 1024, 4)], #, (2**5 * 1024, 2 ** 3 *1024, 4)],
+              ['redpil', 'pillow', 'imageio'])
+    param_names = ['shape', 'mode']
+
+    def setup(self, shape, mode):
+        self.img = np.random.randint(255, size=shape, dtype=np.uint8)
+        self.tmpdir = Path(mkdtemp())
+        self.filename = self.tmpdir / 'saved.bmp'
+        imwrite(self.filename, self.img)
+        self.filename_pillow = self.tmpdir / 'saved_pillow.bmp'
+        # Pillow needs BGRA
+        imwrite(self.filename_pillow, self.img, write_order='BGRA')
+
+
+    def time_save(self, shape, mode):
+        if mode == 'pillow':
+            p = Image.fromarray(self.img)
+            filename = self.tmpdir / 'pillow.bmp'
+            p.save(filename)
+        elif mode == 'imageio':
+            filename = self.tmpdir / 'imageio.bmp'
+            imageio.imwrite(filename, self.img)
+        else:
+            filename = self.tmpdir / 'redpil.bmp'
+            imwrite(filename, self.img)
+
+    def time_load(self, shape, mode):
+        if mode == 'pillow':
+            try:
+                img = np.asarray(
+                    Image.open(self.filename_pillow).convert('RGBA'))
+            except Image.DecompressionBombError:
+                pass
+        elif mode == 'imageio':
+            img = imread(self.filename)
+        else:
+            img = imread(self.filename)
+        assert np.array_equal(img, self.img)
 
     def teardown(self, shape, mode):
         shutil.rmtree(self.tmpdir)
